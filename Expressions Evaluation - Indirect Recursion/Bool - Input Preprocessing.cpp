1fee5a6d65c1b5a767009c3f0150dd7f0c339285
Robert Szarvas <robisz98@gmail.com>
2020-01-18 15:29:26 +0200

    Update and rename Bool - Preprocessing Input.cpp to Bool - Input Preprocessing.cpp

--- Expressions Evaluation - Indirect Recursion/Bool - Input Preprocessing.cpp
+++ Expressions Evaluation - Indirect Recursion/Bool - Input Preprocessing.cpp
@@ -4,16 +4,19 @@
 
 using namespace std;
 
+// Solution 2 of logical expressions evaluation: with input preprocessing
+
 ifstream fin("bool.in");
 ofstream cout("bool.out");
 
 char s[LMAX];
 bool values[27];
 
+// headers of out functions
 int evaluate(char*& expr);
 int operand(char*& expr);
 int value(char*& expr);
-void prelucrare(char*& prel);
+void preprocessing(char*& prel);
 int OR(char*& expr);
 int AND(char*& expr);
 int NOT(char*& expr);
@@ -25,21 +28,30 @@ int main()
     char change;
 	fin.get(s, LMAX - 1);
 	char* prel = s;
-	prelucrare(prel);
+	preprocessing(prel);
 	fin >> n;
 	while(n)
-    {
-        fin >> change;
-        char* expr = s;
-        values[change - 'A'] = !values[change - 'A'];
-        cout<<evaluate(expr);
-        n--;
-    }
+	{
+		fin >> change;
+		char* expr = s;
+		values[change - 'A'] = !values[change - 'A'];
+		cout<<evaluate(expr);
+		n--;
+	}
 
     return 0;
 }
 
-void prelucrare(char*& prel)
+/* 
+Preprocessing means:
+- removing all the spaces
+- AND becomes &
+- OR becomes |
+- NOT becomes !
+- TRUE becomes 1
+- FALSE becomes 0
+*/
+void preprocessing(char*& prel)
 {
 	while (prel[0])
 	{
@@ -78,33 +90,13 @@ void prelucrare(char*& prel)
 	}
 }
 
+// Just calling lowest priority function
 int evaluate(char *& expr)
 {
 	return OR(expr);
 }
 
-int operand(char *& expr)
-{
-    int result;
-    if(expr[0] == '(')
-    {
-        result = evaluate(++expr);
-        ++expr;
-    }
-    else result = value(expr);
-    return result;
-}
-
-int value(char *& expr)
-{
-    char c = expr[0];
-    expr++;
-    if(isdigit(c))
-        return c - '0';
-    else
-        return values[c - 'A'];
-}
-
+// Priority level 3: we'll get out operands by calling the priority level 2 function
 int OR(char *& expr)
 {
     int result = AND(expr);
@@ -115,6 +107,7 @@ int OR(char *& expr)
 	return result;
 }
 
+// Priority level 2: we'll get out operands by calling the priority level 1 function
 int AND(char *& expr)
 {
 	int result = NOT(expr);
@@ -125,6 +118,7 @@ int AND(char *& expr)
 	return result;
 }
 
+// Priority level 1: we'll get out operands directly or proceed recursion if multiple negations are found (e.g. NOT NOT A)
 int NOT(char *& expr)
 {
     int result;
@@ -133,4 +127,28 @@ int NOT(char *& expr)
     else
         result = operand(expr);
     return result;
+}
+
+// Making the decision whether we need to evaluate a sub-expression or extract the value of the operand
+int operand(char *& expr)
+{
+    int result;
+    if(expr[0] == '(')
+    {
+        result = evaluate(++expr); //escape (
+        ++expr; // escape )
+    }
+    else result = value(expr);
+    return result;
+}
+
+// Value function gets way more simple: we can find one of {0, 1, A,...,Z}
+int value(char *& expr)
+{
+    char c = expr[0];
+    expr++; // escape the operand
+    if(isdigit(c))
+        return c - '0';
+    else
+        return values[c - 'A'];
 }
